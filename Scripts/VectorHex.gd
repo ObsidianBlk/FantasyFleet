f13f68a362edce2e2cf4e70b50a7ea0f5fde567d
Bryan Miller <bmiller1008@gmail.com>
2022-05-02 13:42:06 -0400

    A little more code for the VectorHex class. Should now be able to convert Hex to X,Y point and vice versa.

--- Scripts/VectorHex.gd
+++ Scripts/VectorHex.gd
@@ -4,6 +4,12 @@ class_name VectorHex
 # Based on information at...
 # https://www.redblobgames.com/grids/hexagons/
 
+# -------------------------------------------------------------------------
+# Constants and ENUMs
+# -------------------------------------------------------------------------
+const SQRT3 : float = sqrt(3)
+
+enum ORIENTATION {Pointy=0, Flat=1}
 
 # -------------------------------------------------------------------------
 # Variables
@@ -96,11 +102,59 @@ func _ShiftedVectorHex(v : Vector3) -> VectorHex:
 # -------------------------------------------------------------------------
 # Public Methods
 # -------------------------------------------------------------------------
+func is_valid() -> bool:
+	return c.x + c.y + c.z == 0
 
 func distance_to(vh : VectorHex) -> float:
-	var subc : Vector3 = c - vh.qrs
-	return (abs(subc.x) + abs(subc.y) + abs(subc.z)) * 0.5
+	if is_valid() and vh != null and vh.is_valid():
+		var subc : Vector3 = c - vh.qrs
+		return (abs(subc.x) + abs(subc.y) + abs(subc.z)) * 0.5
+	return 0.0
 
+func to_point(orientation : int = ORIENTATION.Pointy) -> Vector2:
+	var x : float = 0.0
+	var y : float = 0.0
+	if is_valid():
+		match orientation:
+			ORIENTATION.Pointy:
+				x = (SQRT3 * c.x) + ((SQRT3/2.0) * c.z)
+				y = (2/3) * c.z
+			ORIENTATION.Flat:
+				x = (2/3) * c.x
+				y = ((SQRT3/2) * c.x) + (SQRT3 * c.z)
+	return Vector2(x,y)
 
-
+func from_point(point : Vector2, orientation : int = ORIENTATION.Pointy) -> void:
+	if not is_valid():
+		return
+	
+	var fq : float = 0.0
+	var fr : float = 0.0
+	match orientation:
+		ORIENTATION.Pointy:
+			fq = ((SQRT3/3) * point.x) - ((1/3) * point.y)
+			fr = (2/3) * point.y
+		ORIENTATION.Flat:
+			fq = (2/3) * point.x
+			fr = ((-1/3) * point.x) + ((SQRT3/3) * point.y)
+	var fs : float = -fq -fr
+	
+	var q = floor(fq)
+	var r = floor(fr)
+	var s = floor(fs)
+	
+	var dq = fq - q
+	var dr = fr - r
+	var ds = fs - s
+	
+	if dq > dr and dq > ds:
+		q = -r -s
+	elif dr > ds:
+		r = -q -s
+	else:
+		s = -r -s
+	
+	c.x = q
+	c.z = r
+	c.y = s
 
