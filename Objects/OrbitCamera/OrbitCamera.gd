547fbdb09b110c13e8fe5144bdd1d33b7f988ab8
Bryan Miller <bmiller1008@gmail.com>
2022-05-06 13:47:38 -0400

    Have a mostly working 3D camera with loose ties to the 2D camera. Refinement required, but a solid start.

--- Objects/OrbitCamera/OrbitCamera.gd
+++ Objects/OrbitCamera/OrbitCamera.gd
@@ -6,22 +6,26 @@ extends Spatial
 export var current : bool = false									setget set_current
 export (int, LAYERS_3D_PHYSICS) var collision_mask : int = 1		setget set_collision_mask
 export (float, 1.0, 180.0, 0.1) var fov : float = 60.0				setget set_fov
+export var near : float = 0.05										setget set_near
+export var far : float = 100.0										setget set_far
 export var min_zoom : float = 0.01
 export var max_zoom : float = 1000.0
 export var zoom_step : float = 2.0
 export var pitch_degree_max : float = 45.0
 export var pitch_degree_min : float = -45.0
-export var smooth_tracking : bool = false
+export var smooth_tracking : bool = false							setget set_smooth_tracking
 export var tracking_duration : float = 0.25
 export var tracking_max_distance : float = 5.0
 
 export var sensitivity : Vector2 = Vector2(0.2, 0.2)
 
-export var target_node_path : NodePath = ""
+export var partner_camera2d_path : NodePath = ""					setget set_partner_camera2d_path
+export var target_node_path : NodePath = ""							setget set_target_node_path
 
 # -----------------------------------------------------------------------------
 # Variables
 # -----------------------------------------------------------------------------
+var _partner_camera_node : Camera2D = null
 var _target_node : Spatial = null
 var _last_target_pos : Vector3 = Vector3.ZERO
 var _tracking_tween : Tween = null
@@ -53,14 +57,42 @@ func set_fov(f : float) -> void:
 		if _camera_node:
 			_camera_node.fov = fov
 
+func set_near(n : float) -> void:
+	if n > 0:
+		near = n
+		if near > far:
+			far = near
+		if _camera_node:
+			_camera_node.near = near
+			_camera_node.far = far
+
+func set_far(f : float) -> void:
+	if f > 0:
+		far = f
+		if far < near:
+			near = far
+		if _camera_node:
+			_camera_node.near = near
+			_camera_node.far = far
+
 func set_target_node_path(tnp : NodePath) -> void:
 	target_node_path = tnp
 	if target_node_path == "":
 		_target_node = null
 	else:
-		var tn = get_node(target_node_path)
+		var tn = get_node_or_null(target_node_path)
 		if tn is Spatial:
 			_target_node = tn
+
+func set_partner_camera2d_path(pnp : NodePath) -> void:
+	partner_camera2d_path = pnp
+	if partner_camera2d_path == "":
+		_partner_camera_node = null
+	else:
+		var c = get_node_or_null(partner_camera2d_path)
+		if c is Camera2D:
+			_partner_camera_node = c
+
 
 func set_smooth_tracking(st : bool) -> void:
 	smooth_tracking = st
@@ -79,6 +111,9 @@ func _ready() -> void:
 	set_current(current)
 	set_collision_mask(collision_mask)
 	set_fov(fov)
+	set_near(near)
+	set_far(far)
+	set_partner_camera2d_path(partner_camera2d_path)
 	set_target_node_path(target_node_path)
 
 func _process(_delta) -> void:
@@ -86,6 +121,11 @@ func _process(_delta) -> void:
 		if _target_node.global_transform.origin != _last_target_pos:
 			_last_target_pos = _target_node.global_transform.origin
 			_TrackToPosition(_last_target_pos)
+	if _partner_camera_node != null:
+		_partner_camera_node.global_position = Vector2(
+			global_transform.origin.x,
+			global_transform.origin.z
+		)
 
 # -----------------------------------------------------------------------------
 # Private Methods
@@ -120,13 +160,24 @@ func set_target(target : Spatial) -> void:
 func move(delta_pos : Vector3) -> void:
 	_TrackToPosition(global_transform.origin + delta_pos)
 
+func move_ground(delta_pos : Vector2) -> void:
+	var dp : Vector3 = Vector3(
+		delta_pos.x * sensitivity.x,
+		0.0,
+		delta_pos.y * sensitivity.y
+	).rotated(Vector3.UP, rotation.y)
+	_TrackToPosition(
+		global_transform.origin + dp
+	)
+
 func orbit(yaw : float, pitch : float) -> void:
 	rotation.y = wrapf(rotation.y - (yaw * sensitivity.x), 0.0, TAU)
 	rotation.x = clamp(rotation.x - (pitch * sensitivity.y), deg2rad(pitch_degree_min), deg2rad(pitch_degree_max))
 
 func zoom(amount : float) -> void:
-	var y = _arm_node.transform.origin.y
-	_arm_node.transform.origin.y = max(max_zoom, min(min_zoom, y - amount))
+	var y = _arm_node.transform.origin.z
+	_arm_node.transform.origin.z = min(max_zoom, max(min_zoom, y - amount))
+	print("Zoom: ", _arm_node.transform.origin.z)
 
 func zoom_in() -> void:
 	zoom(-zoom_step)
--- Scripts/Controllers/CameraCTRL.gd
+++ Scripts/Controllers/CameraCTRL.gd
@@ -12,21 +12,97 @@ export var camera2d_path : NodePath = ""		setget set_camera2d_path
 # -----------------------------------------------------------------------------
 # Variables
 # -----------------------------------------------------------------------------
-var _camera3d_node : Camera = null
+var _camera3d_node : Spatial = null
 var _camera2d_node : Camera2D = null
+
+var _axis_strength : Vector2 = Vector2.ZERO
+var _mod_key : Array = [false, false]
 
 # -----------------------------------------------------------------------------
 # Setters / Getters
 # -----------------------------------------------------------------------------
 func set_camera3d_path(p : NodePath) -> void:
-	pass
+	camera3d_path = p
+	if camera3d_path == "":
+		_camera3d_node = null
+	else:
+		var cn = get_node_or_null(camera3d_path)
+		if cn and cn.has_method("move"):
+			_camera3d_node = cn
 
 func set_camera2d_path(p : NodePath) -> void:
-	pass
+	camera2d_path = p
+	if camera2d_path == "":
+		_camera2d_node = null
+	else:
+		var cn = get_node_or_null(camera2d_path)
+		if cn and cn is Camera2D:
+			_camera2d_node = cn
 
 
 # -----------------------------------------------------------------------------
 # Override Methods
 # -----------------------------------------------------------------------------
+func _ready() -> void:
+	set_camera2d_path(camera2d_path)
+	set_camera3d_path(camera3d_path)
 
+func _unhandled_input(event) -> void:
+	if _camera3d_node == null or _camera2d_node == null:
+		return
+	
+	if event is InputEventMouseMotion:
+		if _mod_key[0]:
+			_camera3d_node.move_ground(-event.relative)
+		elif _mod_key[1]:
+			_camera3d_node.orbit(event.relative.x, event.relative.y)
+	elif event is InputEventJoypadMotion:
+		pass
+	elif event is InputEventMouseButton:
+		match event.button_index:
+			BUTTON_RIGHT:
+				_mod_key[0] = event.pressed or Input.is_action_pressed("game_shift")
+			BUTTON_MIDDLE:
+				_mod_key[1] = event.pressed or Input.is_action_pressed("game_ctrl")
+			BUTTON_WHEEL_UP:
+				print("Wheel Up!")
+				_camera3d_node.zoom_in()
+			BUTTON_WHEEL_DOWN:
+				print("Wheel Down")
+				_camera3d_node.zoom_out()
+	elif event is InputEventAction:
+		match event.action:
+			"game_up":
+				if event.pressed:
+					_axis_strength.y = 0.0 if Input.is_action_pressed("game_down") else -1.0
+				else:
+					_axis_strength.y = 1.0 if Input.is_action_pressed("game_down") else 0.0
+			"game_down":
+				if event.pressed:
+					_axis_strength.y = 0.0 if Input.is_action_pressed("game_up") else 1.0
+				else:
+					_axis_strength.y = -1.0 if Input.is_action_pressed("game_up") else 0.0
+			"game_left":
+				if event.pressed:
+					_axis_strength.x = 0.0 if Input.is_action_pressed("game_right") else -1.0
+				else:
+					_axis_strength.x = 1.0 if Input.is_action_pressed("game_right") else 0.0
+			"game_right":
+				if event.pressed:
+					_axis_strength.x = 0.0 if Input.is_action_pressed("game_left") else 1.0
+				else:
+					_axis_strength.x = -1.0 if Input.is_action_pressed("game_left") else 0.0
+			"game_shift":
+				_mod_key[0] = event.pressed
+			"game_ctrl":
+				_mod_key[1] = event.pressed
+		
+	#if _mod_key[0]:
+	#	_HandleShiftStateEvents(event)
+
+# -----------------------------------------------------------------------------
+# Private Methods
+# -----------------------------------------------------------------------------
+func _HandleShiftStateEvents(event) -> void:
+	pass
 
